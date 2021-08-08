518baa0c0be6cd056345cdbebbc12ce04b9bee84
Erick Saavedra <esvdrah@gmail.com>
2021-08-08 16:10:39 -0500

    End challenge

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Good!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function () {
+    // Close Hamburger Menu
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElements.forEach((element) => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else {
+        // Open Hamburger Menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElements.forEach((element) => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
